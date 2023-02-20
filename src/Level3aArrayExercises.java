a24bbb79633c79c667a7b56b007555224442c3e1
bourrasque <stampixel.t@gmail.com>
2023-02-20 13:13:07 -0500

    Commit on 'master'

---
+++ src/Level3aArrayExercises.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Level3aArrayExercises {
+    public static void main(String[] args) {
+
+    }
+
+    public static void noDuplicates() {
+        Scanner scan = new Scanner(System.in);
+        int[] numbers = new int[7];
+
+        while (true) {
+            int num = scan.nextInt();
+
+        }
+    }
+}
