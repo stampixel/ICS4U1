6f7856a79767b84743e2d26db84f5a74bc6bdebb
bourrasque <stampixel.t@gmail.com>
2023-04-19 15:16:57 -0400

    Commit on 'master'

---
+++ src/SelectionSortAscending.java
@@ -0,0 +1,27 @@
+public class SelectionSortAscending {
+    public static void main(String[] args) {
+        int[] array = {3, 5, 6, 2, 6, 2, 9};
+
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[minIndex] > array[j]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+
+        printArray(array);
+    }
+
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
