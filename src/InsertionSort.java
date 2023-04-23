7483a190f1e4e88041bb222b24df50fe25fcf5c9
bourrasque <stampixel.t@gmail.com>
2023-04-23 17:27:39 -0400

    Commit on 'master'

---
+++ src/InsertionSort.java
@@ -0,0 +1,23 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] array = {3, 5, 6, 2, 6, 2, 9};
+
+        for (int i = 1; i < array.length; i++) {
+            int j = i-1;
+            while (j > 0 && array[j - 1] > array[j]) {
+                int temp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = temp;
+                j = j - 1;
+            }
+        }
+        printArray(array);
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
