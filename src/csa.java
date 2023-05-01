3632b1736ef2ee3f3ee0e7e3f70d62e7a34fbaee
bourrasque <stampixel.t@gmail.com>
2023-04-30 22:22:34 -0400

    Commit on 'master'

--- src/csa.java
+++ src/csa.java
@@ -1,14 +1,11 @@
-static int abs(int x)
-// Retruns the absolute valuie of integer x
-
-static double abs(double x)
-Returns the absolute value of real number x
-
-static double pow(double base, double exp)
-// Retrusn base*exp. Assumes base > 0, or base = 0 and exp > 0, or base < 0 and exp is an integer
-
-static double sqrt(double x)
-Returns sqr(x), x >= 0
-
-static double random()
-// Returns a random number r, where 0.0 <= r <= 1.0
+public void drawLine(int n) {
+    if (n == 0) {
+        System.out.println("That's all, folks!");
+    } else {
+        for (int i = 1; i <= n; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        drawLine(n-1);
+    }
+}
